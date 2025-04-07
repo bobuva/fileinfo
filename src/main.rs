515b771269b5d72d295d13f772aaefa923f3dc22
Bob Uva <ruva3@gatech.edu>
2025-04-07 11:20:31 -0700

    Remove fltk and revert back to console ui only

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use fltk::{app, prelude::*, *};
 
 fn main() {
     if std::env::args().len() < 2 {
-        //println!("usage: fileinfo filename");
-        //return;
-        initialize_ui(None);
+        println!("usage: fileinfo filename");
         return;
     }
 
@@ -27,8 +24,6 @@ fn main() {
     let file = std::fs::OpenOptions::new().read(true).open(filename);
     match file {
         Ok(_) => {
-            initialize_ui(Some(filename));
-
             // dump the contents of the file if it is all UTF-8
             let contents = std::fs::read_to_string(filename).unwrap();
             if contents.is_empty() || contents.is_ascii() || contents.chars().all(|c| c.is_alphabetic()
@@ -44,36 +39,3 @@ fn main() {
     }
 }
 
-fn initialize_ui(filename: Option<&str>) {
-    let title = construct_title(filename);
-
-    let app = app::App::default().with_scheme(app::Scheme::Gtk);
-    let mut win = window::Window::default()
-        .with_size(400, 300)
-        .with_label(&title); // Used the computed title here
-
-    if filename.is_some() {
-        let mut buf = text::TextBuffer::default();
-        let mut txt = text::TextEditor::default()
-            .with_size(400, 200);
-        txt.set_buffer(buf.clone());
-
-        let contents = std::fs::read_to_string(filename.unwrap()).unwrap();
-        buf.set_text(contents.as_str());
-    }
-    win.end();
-    win.show();
-
-
-    app.run().unwrap();
-}
-
-fn construct_title(filename: Option<&str>) -> String {
-    match filename {
-        Some(name) => format!("File Info: {}", name), // Renamed to avoid shadowing
-        None => {
-            println!("No filename provided");
-            "File Info".to_string()
-        }
-    }
-}
