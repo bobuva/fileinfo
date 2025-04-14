4ad85e816976bd4ce49b5fcdb58b26956b9575a8
Bob Uva <ruva3@gatech.edu>
2025-04-14 14:09:37 -0700

    remove unwanted files

--- target/debug/deps/fileinfo.d
+++
@@ -1,5 +0,0 @@
-C:\src\rust\fileinfo\target\debug\deps\fileinfo.exe: src\main.rs
-
-C:\src\rust\fileinfo\target\debug\deps\fileinfo.d: src\main.rs
-
-src\main.rs:
--- target/debug/fileinfo.d
+++
@@ -1,1 +0,0 @@
-C:\src\rust\fileinfo\target\debug\fileinfo.exe: C:\src\rust\fileinfo\src\main.rs
