5572f40b8575a58490130ad385043c710d021ef7
Bob Uva <ruva3@gatech.edu>
2025-04-15 21:09:43 -0700

    used rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ struct Config {
 }
 
 #[derive(Debug)]
-enum FileType {
+pub enum FileType {
     Undefined,
     Text,
     Binary,
@@ -16,7 +16,6 @@ enum FileType {
 // TODO: How to store a list of file extensions that represent a text file? e.g., .txt, .cs, .json
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
@@ -28,7 +27,7 @@ fn main() {
     println!("INSTRUCTION: {}", config.instruction);
     println!("FILENAME: {}", filename);
     println!("-----------------------------");
-    
+
     let mut file_type = FileType::Undefined;
     // assign file type based on extension
     let extension = filename.split('.').last().unwrap();
@@ -42,20 +41,21 @@ fn main() {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str>
-    {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Usage: fileinfo action filename");
         }
         let instruction = args[1].clone();
         let file_name = args[2].clone();
 
-        Ok(Config { instruction, file_name })
+        Ok(Config {
+            instruction,
+            file_name,
+        })
     }
 }
 
-fn process_file(file_name: &str, file_type: FileType) {
-
+fn process_file(file_name: &str, file_type: crate::FileType) {
     println!("Processing file: {}  of type: {:?}", file_name, file_type);
 
     match file_type {
@@ -66,8 +66,7 @@ fn process_file(file_name: &str, file_type: FileType) {
             println!("Dumping contents of file:\n\n");
             dump_file_contents(file_name);
         }
-        FileType::Binary => {
-        }
+        FileType::Binary => {}
     }
 }
 
@@ -77,11 +76,18 @@ fn dump_file_contents(file_name: &str) {
         Ok(_) => {
             // dump the contents of the file if it is all UTF-8
             let contents = std::fs::read_to_string(file_name).unwrap();
-            if contents.is_empty() || contents.is_ascii() || contents.chars().all(|c| c.is_alphabetic()
-                || c.is_ascii() || c.is_numeric() || c.is_whitespace()) {
+            if contents.is_empty()
+                || contents.is_ascii()
+                || contents.chars().all(|c| {
+                    c.is_alphabetic() || c.is_ascii() || c.is_numeric() || c.is_whitespace()
+                })
+            {
                 println!("{}", contents);
             } else {
-                println!("The file, {}, contains non-UTF-8 characters. Exiting.", file_name);
+                println!(
+                    "The file, {}, contains non-UTF-8 characters. Exiting.",
+                    file_name
+                );
             }
         }
         Err(_) => {
